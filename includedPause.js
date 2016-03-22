c023ea5c3fb0a7cb0d2682d43ef79e6d2fc3535a
ryantantiern <ryantantiern@gmail.com>
2016-03-22 21:01:13 +0000

    added message to users

--- includedPause.js
+++ includedPause.js
@@ -1,9 +1,9 @@
 function writeMessage(canvas, message) {
   var context = canvas.getContext('2d');
-  //context.clearRect(0, 0, canvas.width, canvas.height);
-  context.font = '30px Courier New';
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.font = '10px Courier New';
   context.fillStyle = 'black';
-  context.fillText(message, 30, 40);
+  context.fillText(message, 5, 15);
 }
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
@@ -28,15 +28,12 @@ function drawUserLine(){
   if(flag == 0){
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // update userLine conitnuously so that it is never empty
     updateIfNoEntry();
 
     for(i = 0; i < userLine.length - 1; i++){
       // draws the line
       drawLine(userLine[i].x , userLine[i].y, userLine[i+1].x, userLine[i+1].y);
     }
-
-    getValue();
 
 
     for( i = 0; i <userLine.length ; i++){
@@ -48,54 +45,10 @@ function drawUserLine(){
     }
 
     // Never ending loop for continuous function 
-    writeMessage(canvas, "Press e to see waveform. this will end the program.");
-
     setTimeout(function(){drawUserLine();}, 10);
 
   }
 
-}
-
-function getValue() {
-  // push value into final Array
-  var timeElapsed = Date.now() - startTime; 
-  var savedValue ={x: null, y: null, timeStamp: null};
-  savedValue.timeStamp = timeElapsed;
-  $.extend(savedValue, userLine[userLine.length - 1]);
-  trackedWave.push(savedValue); // NOTE :: elements here do not represent a full waveform 
-                                // as the x values have not been corrected to the screen width
-}
-
-function adjustWaveToScreen(uncorrectedWave){
-  // waveform produced is a record of Y-values over time. 
-  // These values are spread over the screen width equally.
-  var dX = canvas.width/uncorrectedWave.length;
-  var correctedX = 0;
-  for(i = 0; i < uncorrectedWave.length; i++){
-    uncorrectedWave[i].x = correctedX;
-    correctedX += dX;
-  }
-  
-}  
-
-function printFinalWaveForm(correctedWave){
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  for(i = 0; i <correctedWave.length - 1; i++){
-    drawLine(correctedWave[i].x , correctedWave[i].y, correctedWave[i+1].x, correctedWave[i+1].y);
-  }
-}                    
-
-var end_DrawUserLine = function(e){
-  console.log("End_Draw_User_Line");
-  if(e.keyCode == 69){
-    flag = 1;
-    e.target.removeEventListener('mousemove', updateUserLine);
-    e.target.removeEventListener('mousedown', handler); // True when paused
-    e.target.removeEventListener('mousedown', pause); // True when not paused
-    adjustWaveToScreen(trackedWave);
-    printFinalWaveForm(trackedWave);
-    e.target .removeEventListener(e.type, arguments.callee);
-  }
 }
 
 function updateIfNoEntry(){
@@ -138,6 +91,23 @@ var pause = function(e){
   e.target.addEventListener('mousedown', handler, false);
 }
 
+/*function storeMousePosFromUserLine(userline) {
+    // This function should take the last element in UserLine
+    // every 2ms and store them in an array.
+    // If userLine is empty, then store a null character
+    var userlineLength = userline.length; 
+
+    if (userline.length != 0) {
+        var mouseDetails = {x: userline[userlineLength - 1].x, y:userline[userlineLength - 1].y};
+    }
+    else {
+        var mouseDetails = null;
+    }
+
+    userWaveForm.push(mouseDetails);
+
+    
+*/
 
 
 var canvas = document.getElementById('myCanvas');
@@ -146,22 +116,24 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-$("body").css("overflow", "hidden"); // Hide scrollbar
-                                     // canvas width and height will be set to the size of window      
-
 var currentPos;
 var lastRecordedPos;
 var userLine = new Array();
-var startTime = Date.now();
-var trackedWave = new Array(); // Raw mouse tracked values
 
-var flag = 0; // turns drawUserLine on or off
+var flag = 0;
 
 canvas.addEventListener('mousedown', handler); 
-window.addEventListener('keydown', end_DrawUserLine);
 
 
+/*var txtFile = "C:\\Users\\Ryan\\Desktop\\myOutputfile.txt";
+var file = new File(txtFile);
+var str = "My string of text";
 
+file.open("w"); // open file with write access
+file.writeln("First line of text");
+file.writeln("Second line of text " + str);
+file.write(str);
+file.close();*/
 
 
 
